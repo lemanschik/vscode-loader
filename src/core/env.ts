f64cd11cdff5ee69f508f9fed23713fa5049b8ef
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2022-12-02 04:00:18 +0000

    refactor_esm

--- src/core/env.ts
+++ src/core/env.ts
@@ -15,7 +15,7 @@
  *---------------------------------------------------------------------------------------------
  *--------------------------------------------------------------------------------------------*/
 
-const _amdLoaderGlobal = this;
+export const _amdLoaderGlobal = globalThis;
 
 declare var module: {
 	exports: any;
@@ -31,11 +31,30 @@ declare var process: {
 		electron: string;
 	}
 };
-declare var require: {
-	nodeRequire(module: string): any;
-};
-declare var global: object;
-const _commonjsGlobal = typeof global === 'object' ? global : {};
+
+declare global {
+	var require: {
+		nodeRequire(module: string): any;
+	};
+	var module: {
+		exports: any;
+	};
+	var process: {
+		platform: string;
+		type: string;
+		mainModule: string;
+		arch: string
+		argv: string[];
+		versions: {
+			node: string;
+			electron: string;
+		}
+	};
+}
+
+
+export const global: any = _amdLoaderGlobal
+export const _commonjsGlobal = typeof global === 'object' ? global : {};
 interface Map<K, V> {
 	clear(): void;
 	delete(key: K): boolean;
@@ -59,70 +78,68 @@ interface MapConstructor {
 }
 declare var Map: MapConstructor;
 
-namespace AMDLoader {
-	export const global: any = _amdLoaderGlobal
 
-	export class Environment {
+export class Environment {
 
-		private _detected: boolean;
-		private _isWindows: boolean;
-		private _isNode: boolean;
-		private _isElectronRenderer: boolean;
-		private _isWebWorker: boolean;
-		private _isElectronNodeIntegrationWebWorker;
+	private _detected: boolean;
+	private _isWindows: boolean;
+	private _isNode: boolean;
+	private _isElectronRenderer: boolean;
+	private _isWebWorker: boolean;
+	private _isElectronNodeIntegrationWebWorker;
 
-		public get isWindows(): boolean {
-			this._detect();
-			return this._isWindows;
-		}
-		public get isNode(): boolean {
-			this._detect();
-			return this._isNode;
-		}
-		public get isElectronRenderer(): boolean {
-			this._detect();
-			return this._isElectronRenderer;
-		}
-		public get isWebWorker(): boolean {
-			this._detect();
-			return this._isWebWorker;
-		}
-		public get isElectronNodeIntegrationWebWorker(): boolean {
-			this._detect();
-			return this._isElectronNodeIntegrationWebWorker;
-		}
+	public get isWindows(): boolean {
+		this._detect();
+		return this._isWindows;
+	}
+	public get isNode(): boolean {
+		this._detect();
+		return this._isNode;
+	}
+	public get isElectronRenderer(): boolean {
+		this._detect();
+		return this._isElectronRenderer;
+	}
+	public get isWebWorker(): boolean {
+		this._detect();
+		return this._isWebWorker;
+	}
+	public get isElectronNodeIntegrationWebWorker(): boolean {
+		this._detect();
+		return this._isElectronNodeIntegrationWebWorker;
+	}
 
-		constructor() {
-			this._detected = false;
-			this._isWindows = false;
-			this._isNode = false;
-			this._isElectronRenderer = false;
-			this._isWebWorker = false;
-			this._isElectronNodeIntegrationWebWorker = false;
-		}
+	constructor() {
+		this._detected = false;
+		this._isWindows = false;
+		this._isNode = false;
+		this._isElectronRenderer = false;
+		this._isWebWorker = false;
+		this._isElectronNodeIntegrationWebWorker = false;
+	}
 
-		private _detect(): void {
-			if (this._detected) {
-				return;
+	private _detect(): void {
+		if (this._detected) {
+			return;
+		}
+		this._detected = true;
+		this._isWindows = Environment._isWindows();
+		this._isNode = (typeof module !== 'undefined' && !!module.exports);
+		this._isElectronRenderer = (typeof process !== 'undefined' && typeof process.versions !== 'undefined' && typeof process.versions.electron !== 'undefined' && process.type === 'renderer');
+		this._isWebWorker = (typeof global.importScripts === 'function');
+		this._isElectronNodeIntegrationWebWorker = this._isWebWorker && (typeof process !== 'undefined' && typeof process.versions !== 'undefined' && typeof process.versions.electron !== 'undefined' && process.type === 'worker');
+	}
+
+	private static _isWindows(): boolean {
+		if (typeof navigator !== 'undefined') {
+			if (navigator.userAgent && navigator.userAgent.indexOf('Windows') >= 0) {
+				return true;
 			}
-			this._detected = true;
-			this._isWindows = Environment._isWindows();
-			this._isNode = (typeof module !== 'undefined' && !!module.exports);
-			this._isElectronRenderer = (typeof process !== 'undefined' && typeof process.versions !== 'undefined' && typeof process.versions.electron !== 'undefined' && process.type === 'renderer');
-			this._isWebWorker = (typeof global.importScripts === 'function');
-			this._isElectronNodeIntegrationWebWorker = this._isWebWorker && (typeof process !== 'undefined' && typeof process.versions !== 'undefined' && typeof process.versions.electron !== 'undefined' && process.type === 'worker');
 		}
-
-		private static _isWindows(): boolean {
-			if (typeof navigator !== 'undefined') {
-				if (navigator.userAgent && navigator.userAgent.indexOf('Windows') >= 0) {
-					return true;
-				}
-			}
-			if (typeof process !== 'undefined') {
-				return (process.platform === 'win32');
-			}
-			return false;
+		if (typeof process !== 'undefined') {
+			return (process.platform === 'win32');
 		}
+		return false;
 	}
 }
+
--- src/core/loaderEvents.ts
+++ src/core/loaderEvents.ts
@@ -3,73 +3,73 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-namespace AMDLoader {
+import { Utilities } from "./utils";
 
-	export const enum LoaderEventType {
-		LoaderAvailable = 1,
+export const enum LoaderEventType {
+	LoaderAvailable = 1,
 
-		BeginLoadingScript = 10,
-		EndLoadingScriptOK = 11,
-		EndLoadingScriptError = 12,
+	BeginLoadingScript = 10,
+	EndLoadingScriptOK = 11,
+	EndLoadingScriptError = 12,
 
-		BeginInvokeFactory = 21,
-		EndInvokeFactory = 22,
+	BeginInvokeFactory = 21,
+	EndInvokeFactory = 22,
 
-		NodeBeginEvaluatingScript = 31,
-		NodeEndEvaluatingScript = 32,
+	NodeBeginEvaluatingScript = 31,
+	NodeEndEvaluatingScript = 32,
 
-		NodeBeginNativeRequire = 33,
-		NodeEndNativeRequire = 34,
+	NodeBeginNativeRequire = 33,
+	NodeEndNativeRequire = 34,
 
-		CachedDataFound = 60,
-		CachedDataMissed = 61,
-		CachedDataRejected = 62,
-		CachedDataCreated = 63,
-	}
-
-	export class LoaderEvent {
-		public type: LoaderEventType;
-		public timestamp: number;
-		public detail: string;
-
-		constructor(type: LoaderEventType, detail: string, timestamp: number) {
-			this.type = type;
-			this.detail = detail;
-			this.timestamp = timestamp;
-		}
-	}
-
-	export interface ILoaderEventRecorder {
-		record(type: LoaderEventType, detail: string): void;
-		getEvents(): LoaderEvent[];
-	}
-
-	export class LoaderEventRecorder implements ILoaderEventRecorder {
-		private _events: LoaderEvent[];
-
-		constructor(loaderAvailableTimestamp: number) {
-			this._events = [new LoaderEvent(LoaderEventType.LoaderAvailable, '', loaderAvailableTimestamp)];
-		}
-
-		public record(type: LoaderEventType, detail: string): void {
-			this._events.push(new LoaderEvent(type, detail, Utilities.getHighPerformanceTimestamp()));
-		}
-
-		public getEvents(): LoaderEvent[] {
-			return this._events;
-		}
-	}
-
-	export class NullLoaderEventRecorder implements ILoaderEventRecorder {
-		public static INSTANCE = new NullLoaderEventRecorder();
-
-		public record(type: LoaderEventType, detail: string): void {
-			// Nothing to do
-		}
-
-		public getEvents(): LoaderEvent[] {
-			return [];
-		}
-	}
-
+	CachedDataFound = 60,
+	CachedDataMissed = 61,
+	CachedDataRejected = 62,
+	CachedDataCreated = 63,
 }
+
+export class LoaderEvent {
+	public type: LoaderEventType;
+	public timestamp: number;
+	public detail: string;
+
+	constructor(type: LoaderEventType, detail: string, timestamp: number) {
+		this.type = type;
+		this.detail = detail;
+		this.timestamp = timestamp;
+	}
+}
+
+export interface ILoaderEventRecorder {
+	record(type: LoaderEventType, detail: string): void;
+	getEvents(): LoaderEvent[];
+}
+
+export class LoaderEventRecorder implements ILoaderEventRecorder {
+	private _events: LoaderEvent[];
+
+	constructor(loaderAvailableTimestamp: number) {
+		this._events = [new LoaderEvent(LoaderEventType.LoaderAvailable, '', loaderAvailableTimestamp)];
+	}
+
+	public record(type: LoaderEventType, detail: string): void {
+		this._events.push(new LoaderEvent(type, detail, Utilities.getHighPerformanceTimestamp()));
+	}
+
+	public getEvents(): LoaderEvent[] {
+		return this._events;
+	}
+}
+
+export class NullLoaderEventRecorder implements ILoaderEventRecorder {
+	public static INSTANCE = new NullLoaderEventRecorder();
+
+	public record(type: LoaderEventType, detail: string): void {
+		// Nothing to do
+	}
+
+	public getEvents(): LoaderEvent[] {
+		return [];
+	}
+}
+
+
